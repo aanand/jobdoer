cf9bd1779f70a8d87548f1d08fc8e5d1f1dab041
Aanand Prasad <aanand.prasad@gmail.com>
2015-11-06 14:11:25 +0000

    Title, axes, colour

--- web/static/app.js
+++ web/static/app.js
@@ -41,18 +41,50 @@ $(function() {
 
     var width = 500;
     var height = 300;
+    var margin = {
+      left: 30, right: 10,
+      bottom: 10, top: 10,
+    }
 
     var x = d3.scale.ordinal()
       .domain(d3.range(0, NUM_SAMPLES))
       .rangeRoundBands([0, width], 0.1);
 
+    var xAxis = d3.svg.axis()
+                  .scale(x)
+                  .tickValues([])
+                  .orient("bottom");
+
     var y = d3.scale.linear()
       .domain([0, d3.max(samples)])
       .rangeRound([height, 0]);
 
-    var svg = d3.select('svg')
+    var yAxis = d3.svg.axis()
+                  .scale(y)
+                  .ticks(2)
+                  .orient("left");
+
+    var svg = d3.select("svg > g")
       .attr('width', width)
       .attr('height', height);
+
+    if (svg.selectAll('g').empty()) {
+      svg = d3.select('svg')
+                .attr("width",  width + margin.left + margin.right)
+                .attr("height", height + margin.top + margin.bottom)
+              .append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      svg.append("g")
+         .attr("class", "x axis")
+         .attr("transform", "translate(0," + height + ")")
+
+      svg.append("g")
+         .attr("class", "y axis")
+    }
+
+    svg.select(".x.axis").call(xAxis);
+    svg.select(".y.axis").call(yAxis);
 
     var bars = svg.selectAll('.bar')
       .data(samples);
